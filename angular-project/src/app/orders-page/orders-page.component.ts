582cd704d59d582f2aa2247c8f8890318a64b3c8
Winlex1 <kaalex822@gmail.com>
2024-12-15 23:19:50 +0100

    Updated UI

--- angular-project/src/app/orders-page/orders-page.component.ts
+++ angular-project/src/app/orders-page/orders-page.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../services/order.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { OrderDTO } from '../order-form/order-form.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-orders-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, DatePipe],
+  providers: [DatePipe],
   templateUrl: './orders-page.component.html',
   styleUrl: './orders-page.component.css'
 })
 export class OrdersPageComponent implements OnInit{
   public ordersData: OrderDTO[] = [];
 
-  constructor(private orderService: OrderService){}
+  currentDate: string;
+
+  constructor(private orderService: OrderService, private datePipe: DatePipe){}
 
   ngOnInit(): void {
     this.orderService.getOrders().subscribe((result) =>{
@@ -21,6 +25,8 @@ export class OrdersPageComponent implements OnInit{
       this.ordersData = result;
     }, (error) =>{
       console.error("An error have occurred while trying to get data of orders", error);
-    })
+    });
+    const now = new Date();
+    this.currentDate = this.datePipe.transform(now, 'dd.MM.yyyy HH:mm:ss');
   }
 }
